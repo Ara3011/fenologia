eaa8b1d7069fe8c8f1e3a845177ebddaae0e6996
israloza <isra.loza15@gmail.com>
2019-12-16 18:34:33 -0600

    Sin middleware

--- app/Http/Controllers/FrutalesController.php
+++ app/Http/Controllers/FrutalesController.php
@@ -11,11 +11,7 @@ use App\Http\Controllers\Response;
 
 class FrutalesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
 
@@ -25,22 +21,12 @@ class FrutalesController extends Controller
 
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(Request $request)
     {
         $validators = Validator::make($request->all(),
@@ -54,35 +40,18 @@ class FrutalesController extends Controller
         return $frutal;
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Frutales  $frutales
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Frutales $frutales)
     {
-        //
+
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Frutales  $frutales
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Frutales $frutales)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Frutales  $frutales
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, Frutales $frutal)
     {
         $validators = Validator::make($request->all(),
@@ -96,15 +65,9 @@ class FrutalesController extends Controller
         return $frutal;
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Frutales  $frutales
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Frutales $frutal)
     {
-        //
         $frutal->delete();
         return response()->json(["message" => "Eliminado correctatemente"], 200);
     }
--- routes/api.php
+++ routes/api.php
@@ -2,21 +2,11 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::resource("frutal", "FrutalesController", ["only" => ["index", "store"]]);
+Route::resource("frutal", "FrutalesController", ["except" => ["create", "edit", "show"]]);
 Route::resource("tipos", "TiposController", ["except" => ["create", "edit", "show"]]);
 Route::resource("eventosextremos", "EventosExtremosController", ["except" => ["create", "edit", "show"]]);
 Route::resource("cultivos", "CultivosController", ["except" => ["create", "edit", "show"]]);
@@ -24,6 +14,7 @@ Route::resource("coniferas", "ConiferasController", ["except" => ["create", "edi
 Route::resource("hojasanchas", "HojasAnchasController", ["except" => ["create", "edit", "show"]]);
 Route::resource("estados", "EstadosController", ["except" => ["create", "edit", "show"]]);
 
-Route::group(['middleware' => ['usuario']], function () {
+/*Route::group(['middleware' => ['usuario']], function () {
     Route::resource("frutal", "FrutalesController", ["only" => ["update", "destroy"]]);
 });
+*/
